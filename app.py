3ffdee046d5210a36b3219c43972c91586622bfd
Syed Saadat Ahmad <s.saadat.ahmad@gmail.com>
2025-02-08 15:17:12 +0530

    dictionany of states and their respective languages

--- app.py
+++ app.py
@@ -2,6 +2,49 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
+state_to_language = {
+    "Andhra Pradesh": "te",  # Telugu
+    "Arunachal Pradesh": "en",  # English (official)
+    "Assam": "as",  # Assamese
+    "Bihar": "hi",  # Hindi
+    "Chhattisgarh": "hi",  # Hindi
+    "Goa": "kok",  # Konkani
+    "Gujarat": "gu",  # Gujarati
+    "Haryana": "hi",  # Hindi
+    "Himachal Pradesh": "hi",  # Hindi
+    "Jharkhand": "hi",  # Hindi
+    "Karnataka": "kn",  # Kannada
+    "Kerala": "ml",  # Malayalam
+    "Madhya Pradesh": "hi",  # Hindi
+    "Maharashtra": "mr",  # Marathi
+    "Manipur": "mni",  # Meitei (Manipuri)
+    "Meghalaya": "en",  # English
+    "Mizoram": "mzn",  # Mizo
+    "Nagaland": "en",  # English
+    "Odisha": "or",  # Odia
+    "Punjab": "pa",  # Punjabi
+    "Rajasthan": "hi",  # Hindi
+    "Sikkim": "ne",  # Nepali
+    "Tamil Nadu": "ta",  # Tamil
+    "Telangana": "te",  # Telugu
+    "Tripura": "bn",  # Bengali
+    "Uttar Pradesh": "hi",  # Hindi
+    "Uttarakhand": "hi",  # Hindi
+    "West Bengal": "bn",  # Bengali
+
+    # Union Territories
+    "Andaman and Nicobar Islands": "hi",  # Hindi
+    "Chandigarh": "hi",  # Hindi (Punjabi & English are also spoken)
+    "Dadra and Nagar Haveli and Daman and Diu": "gu",  # Gujarati
+    "Lakshadweep": "ml",  # Malayalam
+    "Delhi": "hi",  # Hindi
+    "Jammu and Kashmir": "ur",  # Urdu
+    "Ladakh": "bo",  # Ladakhi (Bodhi)
+    "Puducherry": "ta",  # Tamil
+}
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
